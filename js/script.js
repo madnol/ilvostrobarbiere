90c49278bbea455847027ee9f8d93e8650ec4193
Manuel Desole <46155957+madnol@users.noreply.github.com>
2019-07-27 08:31:41 +0200

    Init ilvostrobarbiere

---
+++ js/script.js
@@ -0,0 +1,45 @@
+/*******************************************************************************
+scrollreveal
+*******************************************************************************/
+ScrollReveal().reveal('.reveal', {
+  delay: 500
+});
+
+//stickybar::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+$('.header').sticky({zIndex:9999,});
+
+
+
+
+/*******************************************************************************
+SLIDE ANIMATION
+*******************************************************************************/
+$(document).ready(function() {
+
+  $("#the__Target").skippr({
+    transition: 'fade',
+    speed: 1000,
+    easing: 'easeOutQuart',
+    navType: 'bubble',
+    childrenElementType: 'div',
+    arrows: false,
+    autoPlay: true,
+    autoPlayDuration: 2500,
+    keyboardOnAlways: true,
+    hidePrevious: false
+  });
+  });
+
+//anchorscroll::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+$('.anchor-scroll').anchorScroll({
+  scrollSpeed: 800, // scroll speed
+  offsetTop: 0, // offset for fixed top bars (defaults to 0)
+  onScroll: function() {
+    // callback on scroll start
+  },
+  scrollEnd: function() {
+    // callback on scroll end
+  }
+});
